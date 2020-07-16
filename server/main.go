e9d457d0c6f96f2ab3748a73d1a54d923438adc5
Ryan Johnson <ryan.johnson.1315@gmail.com>
2020-07-16 15:51:20 -0600

    Testing the new workflow

--- server/main.go
+++ server/main.go
@@ -9,6 +9,9 @@ import (
 
 func main() {
 	fmt.Println("USU - UNO v0.0.0")
+	fmt.Println("USU - UNO v0.0.0")
+	fmt.Println("USU - UNO v0.0.0")
+	fmt.Println("USU - UNO v0.0.0")
 
 	// New Echo server
 	e := echo.New()
